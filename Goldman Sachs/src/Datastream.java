948d4bc587c9b31fcf63b30af23e0fa347f752fc
Rbirla07 <ritanshubirla07@gmail.com>
2024-01-03 20:09:15 +0530

    Goldman Sachs Latest commit

---
+++ Goldman Sachs/src/Datastream.java
@@ -0,0 +1,29 @@
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+class DataStream {
+    Queue q ;
+    private int value, k;
+    public DataStream(int value, int k) {
+        q = new LinkedList<>();
+        this.value = value;
+        this.k = k;
+    }
+
+    public boolean consec(int num) {
+        if(num == value){
+            q.add(num);
+        }else{
+            q = new LinkedList<>();
+        }
+        return q.size()>=k;
+    }
+
+}
+
+/**
+ * Your DataStream object will be instantiated and called as such:
+ * DataStream obj = new DataStream(value, k);
+ * boolean param_1 = obj.consec(num);
+ */
